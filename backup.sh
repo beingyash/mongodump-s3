6f056247fb8fd6476f5b74ab919330affa24ddc7
yash <being.yash>
2018-03-05 18:07:52 +0530

    Added slack notification

--- backup.sh
+++ backup.sh
@@ -12,6 +12,7 @@ aws s3 cp "/backup/${BACKUP_NAME}" "s3://${S3_BUCKET}/${S3_PATH}/${BACKUP_NAME}"
 # Delete temp files
 rm -rf /backup/dump
 
+#Slack nontification
 curl -X POST -H 'Content-type: application/json' \
 --data '{"text":"Test slack webhook notification"}' \
 $SLACK_URL
