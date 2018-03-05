81d8804aa9d2dbe9ca3fa2fe5fd3f23c10d0db3d
Yash <yash_2007_1993@yahoo.co.in>
2018-03-05 18:50:45 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -13,7 +13,7 @@ aws s3 cp "/backup/${BACKUP_NAME}" "s3://${S3_BUCKET}/${S3_PATH}/${BACKUP_NAME}"
 rm -rf /backup/dump
 
 #Slack nontification
---echo Sending slack Notification
+echo --Sending slack Notification
 curl -X POST -H 'Content-type: application/json' --data '{"text":"Test slack webhook notification"}' $SLACK_URL
 
 # Delete backup files
