0aa2c40da6016dafc33ad86230d8b1021a0985ec
beingyash <yash_2007_1993@yahoo.co.in>
2018-08-04 20:51:31 +0530

    Added a few echo statements

--- backup.sh
+++ backup.sh
@@ -5,19 +5,21 @@ for MONGO_URI in $MONGO_URIS; do
 
 #Extracting dbname from MONGO_URI	
 DBNAME=$(echo ${MONGO_URI} | sed "s/.*[0-9]\///")
-
 #Name of the compressed backup file
 BACKUP_NAME="${DBNAME}-$(date -u +%Y-%m-%d_%H-%M-%S)_UTC.gz"
 
+echo "--Starting backup of ${DBNAME}"
 # Run backup
 mongodump --uri=${MONGO_URI} --gzip -o /backup/${DBNAME}
 
+echo "--Compressing ${DBNAME} backup"
 # Compress backup
 cd /backup/${DBNAME} && tar -cvzf "${BACKUP_NAME}" ${DBNAME}
 
 
 if [ "$S3_UPLOAD" = "true" ]; then
 
+echo "--Uploading ${DBNAME} to s3 bucket ${S3_BUCKET}"
 # Upload backup
 #aws s3 cp "/backup/${DBNAME}/${BACKUP_NAME}" "s3://${S3_BUCKET}/${S3_PATH}/${BACKUP_NAME}"
 aws s3 cp "/backup/${DBNAME}/${BACKUP_NAME}" "s3://${S3_BUCKET}/${BACKUP_NAME}"
@@ -27,9 +29,9 @@ rm -rf /backup/${DBNAME}
 fi
 
 #Slack nontification
-echo --Sending slack Notification for $DBNAME
+echo --Sending slack Notification for ${DBNAME}
 curl -X POST -H 'Content-type: application/json' \
---data '{"text":"'$DBNAME' backup done and uploaded to S3"}' \
+--data '{"text":"${DBNAME} backup done and uploaded to S3"}' \
 $SLACK_URL
 
 # Delete backup files
