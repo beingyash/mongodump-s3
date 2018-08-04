9af9f23128f7203c557ceb80099f02cb435edc46
beingyash <yash_2007_1993@yahoo.co.in>
2018-08-04 17:05:53 +0530

    completed script for multiple db backups

--- backup.sh
+++ backup.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env sh
 
-#OPTIONS=`python /usr/local/bin/mongouri`
-BACKUP_NAME="$(date -u +%Y-%m-%d_%H-%M-%S)_UTC.gz"
+
+for MONGO_URI in $MONGO_URIS; do
+
+#Extracting dbname from MONGO_URI	
+DBNAME=$(echo ${MONGO_URI} | sed "s/.*[0-9]\///")
+
+#Name of the compressed backup file
+BACKUP_NAME="${DBNAME}-$(date -u +%Y-%m-%d_%H-%M-%S)_UTC.gz"
 
 # Run backup
-mongodump --uri=$MONGO_URI --gzip -o /backup/dump
+mongodump --uri=${MONGO_URI} --gzip -o /backup/${DBNAME}
+
 # Compress backup
-cd /backup/ && tar -cvzf "${BACKUP_NAME}" dump
+cd /backup/${DBNAME} && tar -cvzf "${BACKUP_NAME}" ${DBNAME}
+
+
+if [ "$S3_UPLOAD" = "true" ]; then
+
 # Upload backup
-aws s3 cp "/backup/${BACKUP_NAME}" "s3://${S3_BUCKET}/${S3_PATH}/${BACKUP_NAME}"
+#aws s3 cp "/backup/${DBNAME}/${BACKUP_NAME}" "s3://${S3_BUCKET}/${S3_PATH}/${BACKUP_NAME}"
+aws s3 cp "/backup/${DBNAME}/${BACKUP_NAME}" "s3://${S3_BUCKET}/${BACKUP_NAME}"
 # Delete temp files
-rm -rf /backup/dump
+rm -rf /backup/${DBNAME}
+
+fi
 
 #Slack nontification
 echo --Sending slack Notification for $DBNAME
@@ -20,11 +34,13 @@ $SLACK_URL
 
 # Delete backup files
 if [ -n "${MAX_BACKUPS}" ]; then
-  while [ $(ls /backup -w 1 | wc -l) -gt ${MAX_BACKUPS} ];
+  while [ $(ls /backup/${DBNAME} -w 1 | wc -l) -gt ${MAX_BACKUPS} ];
   do
     BACKUP_TO_BE_DELETED=$(ls /backup -w 1 | sort | head -n 1)
-    rm -rf /backup/${BACKUP_TO_BE_DELETED}
+    rm -rf /backup/${DBNAME}/${BACKUP_TO_BE_DELETED}
   done
 else
-  rm -rf /backup/*
+  rm -rf /backup/${DBNAME}/*
 fi
+
+done
